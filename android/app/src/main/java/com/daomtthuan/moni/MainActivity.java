2c67433b9d5d9961ae87884b321d6188fa312eab
Daomtthuan <dao.mt.thuan@gmail.com>
2022-10-08 13:58:36 +0700

    upgrade dependencies and add react-navigation

--- android/app/src/main/java/com/daomtthuan/moni/MainActivity.java
+++ android/app/src/main/java/com/daomtthuan/moni/MainActivity.java
@@ -10,9 +10,9 @@ import org.devio.rn.splashscreen.SplashScreen;
 
 public class MainActivity extends ReactActivity {
   @Override
-  protected void onCreate(Bundle savedInstanceState) {
+  protected void onCreate() {
     SplashScreen.show(this);
-    super.onCreate(savedInstanceState);
+    super.onCreate();
   }
 
   /**
